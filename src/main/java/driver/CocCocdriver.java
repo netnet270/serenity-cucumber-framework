df386fefe1e92ea2b0be5b6dcf9c9d85419b1c53
tratranguyen-automation <78290600+tratranguyen-automation@users.noreply.github.com>
2022-08-23 11:26:22 +0700

    add commond run browser on coccoc

--- src/main/java/driver/CocCocdriver.java
+++ src/main/java/driver/CocCocdriver.java
@@ -1,2 +1,46 @@
-package driver;public class CocCocdriver {
+package driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import net.thucydides.core.webdriver.DriverSource;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CocCocdriver implements DriverSource {
+    @Override
+    public WebDriver newDriver() {
+        try {
+            Map<String, Object> chromePrefs = new HashMap<String, Object>();
+            chromePrefs.put("download.default_directory", "downLoadDirectory");
+            chromePrefs.put("profile.default_content_settings.popups", 0);
+            chromePrefs.put("pdfjs.disabled", true);
+
+            final DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+            capabilities.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
+
+            WebDriverManager.chromedriver().driverVersion("104.0.5112.79").setup();
+            ChromeOptions options = new ChromeOptions();
+            options.setBinary("C:/program Files/CocCoc/Browser/Application/browser.exe");
+
+            options.addArguments("--start-maximized");
+            options.setExperimentalOption("prefs", chromePrefs);
+            options.merge(capabilities);
+
+            return new ChromeDriver(options);
+        } catch (Exception e) {
+            throw new Error(e);
+        }
+    }
+
+
+    @Override
+    public boolean takesScreenshots() {
+        return true;
+    }
 }
--- src/test/java/coccoc/AM/definition/campaigns/CampaignCreateDef.java
+++ src/test/java/coccoc/AM/definition/campaigns/CampaignCreateDef.java
@@ -5,6 +5,7 @@ import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import net.thucydides.core.annotations.Steps;
 
+
 import java.util.List;
 import java.util.Map;
 
@@ -32,8 +33,9 @@ public class CampaignCreateDef {
     public void input_data_campaigns_to_create(DataTable dataTable) {
         List<Map<String,String>> data = dataTable.asMaps(String.class, String.class);
         for (Map<String, String> row : data) {
-            String campaignName = row.get("Campaign name");
             String timePeriod = row.get("Time period");
+            String campaignName = row.get("Campaign name");
+            System.out.println(campaignName);
             String bidStrategy = row.get("Bid strategy");
             String dailyLimit = row.get("Daily limit");
             campaignCreateSteps.inputDataToFieldWithLabel("Enter campaign name", campaignName + campaignCreateSteps.randomInteger());
